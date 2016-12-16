599fb194edb35bee68d03ef0beac33e398979f37
MATSUMOTO Ryosuke <matsumotory@pepabo.com>
2016-12-16 18:28:06 +0900

    Add comment

--- example/shared_ptr.c
+++ example/shared_ptr.c
@@ -42,13 +42,19 @@ int main(int argc, char *argv[])
   mrb_define_class_method(mrb_dst, pter_dst, "set", mrb_pter_set, MRB_ARGS_REQ(1));
   mrb_define_class_method(mrb_dst, pter_dst, "get", mrb_pter_get, MRB_ARGS_NONE());
 
+  /* create a shared ptr object */
   mrb_ud_ptr_init(mrb_src);
 
+  /* set ptr on mrb_src into the shared ptr object */
   mrb_load_string(mrb_src, pter_set_code);
 
+  /* connect the shared ptr object mrb_src with mrb_dst */
   mrb_ud_ptr_copy(mrb_src, mrb_dst);
 
+  /* get ptr on mrb_src from shared ptr object on mrb_dst */
   mrb_load_string(mrb_dst, pter_get_code);
+
+  mrb_ud_ptr_free(mrb_src);
 
   mrb_close(mrb_src);
   mrb_close(mrb_dst);
