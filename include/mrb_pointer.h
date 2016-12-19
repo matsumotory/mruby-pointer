57ebc6bc4446ef63bcedcd45542d22d9a99cd645
matsumotory <matsu1229@gmail.com>
2016-12-19 16:21:26 +0900

    Add comments and new APIs

--- include/mrb_pointer.h
+++ include/mrb_pointer.h
@@ -9,10 +9,23 @@
 
 #include "mruby.h"
 
-void mrb_udptr_init(mrb_state *mrb);
-void mrb_udptr_set(mrb_state *mrb, void *ptr);
+/* create shared ptr object */
+void mrb_udptr_init(mrb_state *mrb_src);
+
+/* set ptr on mrb_src into shared ptr object */
+void mrb_udptr_set(mrb_state *mrb_src, void *ptr);
+
+/* get ptr from shared ptr object */
 void *mrb_udptr_get(mrb_state *mrb);
+
+/* copy ptr from mrb_src to mrb_dst */
 void mrb_udptr_copy(mrb_state *mrb_src, mrb_state *mrb_dst);
+
+/* get src/dst mrb_state from shared ptr object */
+mrb_state *mrb_udptr_src_mrb(mrb_state *mrb);
+mrb_state *mrb_udptr_dst_mrb(mrb_state *mrb);
+
+/* free shared ptr object */
 void mrb_udptr_free(mrb_state *mrb);
 
 #endif
