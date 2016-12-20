ac08f97baa88188c2e3e4d7b18028b9d00343a6a
MATSUMOTO Ryosuke <matsumotory@pepabo.com>
2016-12-20 15:05:01 +0900

    Add test for redis which shared between mrb_states

--- bintest/mrb_pointer.rb
+++ bintest/mrb_pointer.rb
@@ -2,3 +2,6 @@ assert('mruby-pointer bin test') do
   assert_equal "I am pter\n", `../bintest/shared_ptr`
 end
 
+assert('mruby-pointer bin test for redis') do
+  assert_equal "\"foo\"\n", `../bintest/shared_redis_ctx`
+end
--- build_config.rb
+++ build_config.rb
@@ -2,6 +2,7 @@ MRuby::Build.new do |conf|
   toolchain :gcc
   conf.gembox 'full-core'
   conf.gem File.expand_path(File.dirname(__FILE__))
+  conf.gem :mgem => 'mruby-redis'
   conf.enable_test
   conf.enable_bintest
 end
